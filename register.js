736b50923b3d1cb04f149e47017fd9f7901c890d
JT <Master120344@yahoo.com>
2024-08-02 05:02:18 -0500

    Update register.js

--- register.js
+++ register.js
@@ -5,17 +5,8 @@ document.addEventListener('DOMContentLoaded', () => {
         const username = document.getElementById('username').value;
         const password = document.getElementById('password').value;
         const email = document.getElementById('email').value;
+        
         const user = {
             username: username,
             password: password,
-            email: email,
-            balance: 0
-        };
-        console.log("User:", user); // Debugging
-        localStorage.setItem('user', JSON.stringify(user));
-        document.getElementById('confirmationMessage').style.display = 'block';
-        setTimeout(() => {
-            window.location.href = 'https://master120344.github.io/RealSurveys/welcome.html';
-        }, 2000);
-    });
-});
+            email:
