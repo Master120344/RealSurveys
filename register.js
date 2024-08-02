0669b9ab930d0cd42c7d1dac0ae6cb88831bfef7
JT <Master120344@yahoo.com>
2024-08-02 05:40:33 -0500

    Update register.js

--- register.js
+++ register.js
@@ -1,11 +1,17 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const form = document.getElementById('registerForm');
-    form.addEventListener('submit', (event) => {
-        event.preventDefault();
-        const username = document.getElementById('username').value;
-        const password = document.getElementById('password').value;
-        const email = document.getElementById('email').value;
-        const user = {
-            username: username,
-            password: password,
-            email: email
+function registerUser(event) {
+    event.preventDefault();
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+    const email = document.getElementById('email').value;
+    const user = {
+        username,
+        password,
+        email,
+        balance: 0
+    };
+    localStorage.setItem('user', JSON.stringify(user));
+    document.querySelector('.redirecting').style.display = 'block';
+    setTimeout(() => {
+        window.location.href = 'welcome.html';
+    }, 2000);
+}
