d0b9bd66625accbfdd838d15df9e16f4c018ceab
JT <Master120344@yahoo.com>
2024-08-14 08:05:50 -0700

    Update welcome.js

--- welcome.js
+++ welcome.js
@@ -1,13 +1,18 @@
 firebase.auth().onAuthStateChanged((user) => {
-if (user) {
-const userId = user.uid;
-firebase.database().ref('users/' + userId).on('value', (snapshot) => {
-const userData = snapshot.val();
-document.getElementById('username').innerHTML = userData.username;
-document.getElementById('email').innerHTML = userData.email;
-document.getElementById('balance').innerHTML = userData.balance;
-});
-} else {
-window.location.href = 'index.html';
-}
+  if (user) {
+    const userId = user.uid;
+    firebase.database().ref('users/' + userId).on('value', (snapshot) => {
+      const userData = snapshot.val();
+      document.getElementById('username').innerHTML = userData.username;
+      document.getElementById('email').innerHTML = userData.email;
+      document.getElementById('balance').innerHTML = userData.balance;
+    });
+    document.getElementById('logout-btn').addEventListener('click', () => {
+      firebase.auth().signOut().then(() => {
+        window.location.href = 'index.html';
+      });
+    });
+  } else {
+    window.location.href = 'index.html';
+  }
 });
