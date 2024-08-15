64f3a834172ac6f35509e3ec33d0c98992fa102b
JT <Master120344@yahoo.com>
2024-08-14 17:21:19 -0700

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,7 +1,11 @@
+// firebase-config.js
+
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getStorage } from "firebase/storage";
 
+// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyA7GP-4bnijUNXGBti2nCOJF9iwusuL7c4",
   authDomain: "real-surveys.firebaseapp.com",
@@ -11,8 +15,10 @@ const firebaseConfig = {
   appId: "1:1024139519354:web:a0b11a5a0560ab02ee22c3"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const storage = getStorage(app);
 
+// Export auth and storage to use in other files
 export { auth, storage };
