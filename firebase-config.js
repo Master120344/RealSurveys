2994ea94f66ea76e41377230e47a443f2c3f1d8f
JT <Master120344@yahoo.com>
2024-08-14 18:48:24 -0700

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,18 +1,16 @@
-// firebase-config.js
-
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
-import { getStorage } from "firebase/storage";
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+import { getStorage } from 'firebase/storage';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyA7GP-4bnijUNXGBti2nCOJF9iwusuL7c4",
-  authDomain: "real-surveys.firebaseapp.com",
-  projectId: "real-surveys",
-  storageBucket: "real-surveys.appspot.com",
-  messagingSenderId: "1024139519354",
-  appId: "1:1024139519354:web:a0b11a5a0560ab02ee22c3"
+  apiKey: 'AIzaSyA7GP-4bnijUNXGBti2nCOJF9iwusuL7c4',
+  authDomain: 'real-surveys.firebaseapp.com',
+  projectId: 'real-surveys',
+  storageBucket: 'real-surveys.appspot.com',
+  messagingSenderId: '1024139519354',
+  appId: '1:1024139519354:web:a0b11a5a0560ab02ee22c3'
 };
 
 // Initialize Firebase
