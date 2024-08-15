095d08290961b12f566fd97a74d73082caa37ce5
JT <Master120344@yahoo.com>
2024-08-15 00:35:00 -0700

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,10 +1,8 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.22.0/firebase-app.js';
 import { getAuth } from 'https://www.gstatic.com/firebasejs/9.22.0/firebase-auth.js';
 import { getStorage } from 'https://www.gstatic.com/firebasejs/9.22.0/firebase-storage.js';
-import { getDatabase } from 'https://www.gstatic.com/firebasejs/9.22.0/firebase-database.js'; // Import Realtime Database
+import { getDatabase } from 'https://www.gstatic.com/firebasejs/9.22.0/firebase-database.js';
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: 'AIzaSyA7GP-4bnijUNXGBti2nCOJF9iwusuL7c4',
   authDomain: 'real-surveys.firebaseapp.com',
@@ -14,11 +12,9 @@ const firebaseConfig = {
   appId: '1:1024139519354:web:a0b11a5a0560ab02ee22c3'
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const storage = getStorage(app);
-const database = getDatabase(app); // Initialize Realtime Database
+const database = getDatabase(app);
 
-// Export auth, storage, and database to use in other files
 export { auth, storage, database };
